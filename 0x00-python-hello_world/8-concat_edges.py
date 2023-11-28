d686069fd142afcc1c7f60ac6f197e8a2b7b508f
Ian-Kimeli <iankimeli014@gmail.com>
2023-11-27 17:31:43 -0800

    All files done to succesful output, including advanced tasks

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an intepreted, interactive, object-oriented programming\
+        languae that combines remarkable power with very clear syntax"
+str = str[38:67] + str[107:112] + str[:0]
+print(str)
