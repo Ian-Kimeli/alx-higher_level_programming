2cad3caf5539706026e723d51cf0920e238b48ae
Ian-Kimeli <iankimeli014@gmail.com>
2023-12-01 01:59:42 -0800

    File 1 corrections

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
-#!/usr/bin/ython3
+#!/usr/bin/python3
 
-if __name__ == "__main__"
-    """print the sum, difference, mutiple and  quotient of 10 and 5. """
-    from claculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    """print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
