229d578771441b14ea1d3d1bc90ba2fad5a4fbc2
Ian-Kimeli <iankimeli014@gmail.com>
2024-01-05 02:08:56 -0800

    All files commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_nme'.
+    """
+
+    __slots__ = ["first_name"]
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,6 @@
+# Python - Everything is object
+
+0x09 File
+Objects are nothing but an attribute or key to understanding object-oriented technology.
+
+
